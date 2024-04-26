ba1acfd7dfba14e5a28c6fd17c7ed46c5e664e35
EdgyBean <shadowowlsoul@gmail.com>
2024-04-25 21:03:38 -0500

    Started Tutorial Sequence
    Still stuck on coding in event triggers, but dialogue and fade in-and-out animation being set up

---
+++ Assets/Self Made/Tutorial Sequence/TutorialInteraction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialInteraction : MonoBehaviour
+
+    //private Animator animator;
+    // for fade to black after drinking
+
+{
+    
+    //gameObject.GetComponent<TutorialInteraction>().InteractReciever();
+}
