f761a2464e38400ba9d8cab0b7730ea3a5b7ad6c
Agoodman02 <agoodman880@gmail.com>
2024-04-28 17:15:02 -0500

    Locking Camera
    Locks camera and player movement when the player interacts with characters.

--- Assets/Scripts/InteractNPC/PlayerInteract.cs
+++ Assets/Scripts/InteractNPC/PlayerInteract.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Numerics;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-   PlayerControler playerControler;
+   
    PlayerControler player;
+
+   UnityEngine.Vector3 myVector;
+   Rigidbody m_Rigidbody;
+
+   public bool enterCollider= false;
+
+
+   private void Start() 
+   {
+        myVector = new UnityEngine.Vector3(0.0f,0.0f,0.0f);
+        m_Rigidbody = GetComponent<Rigidbody>();
+   }
+
+   private void Awake()
+   {
+        player = PlayerControler.player;
+   }
 
    private void Update() 
    {
@@ -16,38 +36,55 @@ public class PlayerInteract : MonoBehaviour
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             foreach (Collider collider in colliderArray)
             {   
-                Debug.Log(collider);
-                if(collider.TryGetComponent(out NPCInteractable Bartholomew_Vampire)) //Bart Main
-                    {
-                        //playerControler.TeleportPlayer(Vector3, Vector3,false);
-                        Bartholomew_Vampire.StartConversation();
-                    } 
-                if(collider.TryGetComponent(out NPCInteractable Edmund_Skeleton)) //Edmund Main
-                    {
-                        //playerControler.TeleportPlayer(Vector3, Vector3, false);
-                        Edmund_Skeleton .StartConversation();
-                    } 
-                if(collider.TryGetComponent(out NPCInteractable Max_Werewolf)) //Maxwell Main
-                    {
-                        //playerControler.TeleportPlayer(Vector3, Vector3,false);
-                        Max_Werewolf.StartConversation();
-                    } 
-                if(collider.TryGetComponent(out NPCInteractable Minerva_Witch)) //Minerva Main
-                    {
-                        //playerControler.TeleportPlayer(Vector3, Vector3,false);
-                        Minerva_Witch.StartConversation();
-                    } 
-                if(collider.TryGetComponent(out NPCInteractable MrWraithwood)) //Wraithwood Main
-                    {
-                        //playerControler.TeleportPlayer(Vector3, Vector3, false);
-                        MrWraithwood.StartConversation();
-                    } 
+                Debug.Log(collider.gameObject.name);
+                if (enterCollider == true)
+                {
+                    //player.DoCameraControl = false;
+                    //if(collider.TryGetComponent(out NPCInteractable Bartholomew_Vampire)) //Bart Main
+                    if (collider.gameObject.name == "Bartholomew_Vampire") // BartMain.yarn
+                        {
+                            collider.TryGetComponent<NPCInteractable>(out NPCInteractable Bartholomew_Vampire);
+                            Debug.Log("Interact with Bart");
+                            player.TeleportPlayer(myVector, myVector, false);
+                            Bartholomew_Vampire.StartConversation();
+                        } 
+                    if (collider.gameObject.name == "Edmund_Skeleton") // EdmundMain.yarn
+                        {
+                            collider.TryGetComponent<NPCInteractable>(out NPCInteractable Edmund_Skeleton);
+                            Debug.Log("Interact with Edmund");
+                            player.TeleportPlayer(myVector, myVector, false);
+                            Edmund_Skeleton.StartConversation();
+                        } 
+                    if (collider.gameObject.name == "Max_Werewolf") // MaxwellMain.yarn
+                        {
+                            collider.TryGetComponent<NPCInteractable>(out NPCInteractable Max_Werewolf);
+                            Debug.Log("Interact with Maxwell");
+                            player.TeleportPlayer(myVector, myVector, false);
+                            Max_Werewolf.StartConversation();
+                        } 
+                    if (collider.gameObject.name == "Minerva_Witch") //Minerva Main
+                        {
+                            collider.TryGetComponent<NPCInteractable>(out NPCInteractable Minerva_Witch);
+                            Debug.Log("Interact with Minerva");
+                            player.TeleportPlayer(myVector, myVector, false);
+                            Minerva_Witch.StartConversation();
+                        } 
+                    if (collider.gameObject.name == "Mr.Wraithwood") //Wraithwood Main
+                        {
+                            collider.TryGetComponent<NPCInteractable>(out NPCInteractable MrWraithwood);
+                            Debug.Log("Interact with Wraithwood");
+                            player.TeleportPlayer(myVector, myVector, false);
+                            MrWraithwood.StartConversation();
+                        } 
+                }
             }
-        }
-    playerControler.DoCameraControl = true;
+            player.DoCameraControl = true;
+        }  
    }
+    
 
     //UI
+    
    public NPCInteractable GetNPCInteractableObject() 
    {
        float interactRange = 2f;
@@ -61,6 +98,18 @@ public class PlayerInteract : MonoBehaviour
            }
         }
         return null;
+   }
+   
+   private void OnTriggerEnter(Collider other) 
+   {
+        Debug.Log("By character");
+        enterCollider= true;
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+        Debug.Log("Exit character");
+        enterCollider= false;
    }
 
 }
